6a171519667ced0df3a7819a11d406d118d91459
Emilie Morissette <emorissettegregoire@gmail.com>
2019-02-18 16:46:08 -0500

    Device gem installed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_191619) do
+ActiveRecord::Schema.define(version: 2019_02_18_211021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2019_02_18_191619) do
     t.bigint "apartment_id"
     t.string "enter_date"
     t.string "exit_date"
-    t.string "price_total"
+    t.integer "price_total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["apartment_id"], name: "index_bookings_on_apartment_id"
@@ -41,14 +41,20 @@ ActiveRecord::Schema.define(version: 2019_02_18_191619) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.boolean "owner"
+    t.boolean "owner", default: false
     t.string "first_name"
     t.string "last_name"
-    t.string "email"
     t.string "photos"
     t.string "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "apartments", "users"
