817b020e33e0d9dd6f5d4218000f001cf839a3f0
Jamie Webber <jamiecwebber@gmail.com>
2019-02-18 16:09:50 -0500

    finished bookings_controller, probably unstable

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,22 +1,47 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = Booking.all
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def new
+    @user = User.find(params[:user_id])
+    @booking = Booking.new
+    # link to bookings#new needs to send an :apartment_id parameter
+    @apartment = params[:apartment_id]
   end
 
   def create
+    @user = User.find(params[:user_id])
+    @apartment = Apartment.find(params([:apartment_id]))
+    @booking = Booking.new(booking_params)
+    @booking.user = @user
+    @booking.apartment = @apartment
+    if @booking.save
+      redirect_to user_booking_path(@booking)
+    else
+      :new
+    end
   end
 
   def edit
+    @booking = Booking.find(params[:id])
   end
 
   def update
+    @booking = Booking.find(params[:id])
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:enter_date, :exit_date, :price_total)
   end
 end
