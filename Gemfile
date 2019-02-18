606997eda7c3a303d9f6a7fd3ca2f37eca88a22a
Richard Jarram <rjarram@me.com>
2019-02-18 18:06:58 -0500

    seed v1.0 finished

--- Gemfile
+++ Gemfile
@@ -1,6 +1,10 @@
 source 'https://rubygems.org'
 ruby '2.5.3'
 
+# Richard Jarram's GEMS
+gem 'faker'
+gem 'rest-client'
+
 gem 'bootsnap', require: false
 gem 'jbuilder', '~> 2.0'
 gem 'pg', '~> 0.21'
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,28 @@ def bio_generator
   result = api["slip"]["advice"]
 end
 
+# Generates an apartment that is run when you create a new host object.
+def generate_appartment(id)
+  host_id = id
+  price_per_day = rand(20..500)
+  category = ["arts", "sports", "science", "business"]
+  name = Faker::Restaurant.name
+  description = bio_generator # See line 15 for method definition
+  location = "#{Faker::Address.street_address}, #{Faker::Address.city}, #{Faker::Address.zip_code}"
+  photos = "https://source.unsplash.com/collection/494266/1600x900?#{rand()}"
+
+  new_appartment = Apartment.new({
+    price_per_day: price_per_day,
+    category: category[rand(0..3)],
+    name: name,
+    description: description,
+    location: location,
+    photos: photos,
+    user_id: host_id
+  })
+  new_appartment.save
+end
+
 # Returns an array of 100 profiles
 def user_api_call
   api_key = "33d7ed1cc08d6f5586e8dd45dd409b"
@@ -27,50 +49,61 @@ def user_api_call
   response = RestClient.post(
     url,
     request,
-    :content_type => :json, :accept => :json, :'X-API-KEY' => user_api_key)
+    :content_type => :json, :accept => :json, :'X-API-KEY' => api_key)
   result_array = JSON.parse(response)
 end
 
 # Splitting 100 users into 'guests' and 'owners'
 array_of_users = user_api_call
-guests = array_of_users[0..50]
-hosts = array_of_users[51..100]
+guests = array_of_users[0...49]
+hosts = array_of_users[49..99]
 
 # Generating 50 user objects and saving to database.
 guests.each do |user|
-  owner = false
   first_name = user["name"].split(" ")[0]
   last_name = user["name"].split(" ")[1]
   email = user["email"]
   bio = bio_generator # See line 15 for method definition
   photos = user["photo"]
 
-  new_user = @User.new(
+  new_user = User.new({
+    owner: false,
     first_name: first_name,
     last_name: last_name,
     email: email,
     bio: bio,
     photos: photos
-    )
+    })
   new_user.save
 end
 
 # Generating 50 host objects and saving to database.
 hosts.each do |user|
-  owner = true
   first_name = user["name"].split(" ")[0]
   last_name = user["name"].split(" ")[1]
   email = user["email"]
   bio = bio_generator # See line 15 for method definition
   photos = user["photo"]
 
-  new_user = @User.new(
+  new_user = User.new({
+    owner: true,
     first_name: first_name,
     last_name: last_name,
     email: email,
     bio: bio,
     photos: photos
-    )
+  })
   new_user.save
+
+  generate_appartment(new_user.id)
 end
+
+
+
+
+
+
+
+
+
 
