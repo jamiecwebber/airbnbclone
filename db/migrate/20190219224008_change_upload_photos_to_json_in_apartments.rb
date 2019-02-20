889b8179efecf19745e1d779c524075e7f7d78b0
Richard Jarram <rjarram@me.com>
2019-02-20 10:51:12 -0500

    deleted convert uploadphotostojson migration - redundant

--- db/migrate/20190219224008_change_upload_photos_to_json_in_apartments.rb
+++
@@ -1,6 +0,0 @@
-class ChangeUploadPhotosToJsonInApartments < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :apartments, :upload_photos
-    add_column :apartments, :upload_photos, :json
-  end
-end
