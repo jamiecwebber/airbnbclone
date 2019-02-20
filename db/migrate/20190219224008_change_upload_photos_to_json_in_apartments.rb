d0b740403d9d4ec983ae14c0f011ede64a95a85c
Emilie Morissette <emorissettegregoire@gmail.com>
2019-02-20 10:51:52 -0500

    deleted convert uploadphotostojson migration - redundant

--- db/migrate/20190219224008_change_upload_photos_to_json_in_apartments.rb
+++ db/migrate/20190219224008_change_upload_photos_to_json_in_apartments.rb
@@ -1,6 +1,4 @@
 class ChangeUploadPhotosToJsonInApartments < ActiveRecord::Migration[5.2]
   def change
-    remove_column :apartments, :upload_photos
-    add_column :apartments, :upload_photos, :json
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_174056) do
+ActiveRecord::Schema.define(version: 2019_02_19_224008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2019_02_19_174056) do
     t.string "photos"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "upload_photos"
+    t.json "upload_photos"
     t.index ["user_id"], name: "index_apartments_on_user_id"
   end
 
