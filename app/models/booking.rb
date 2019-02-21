4b66198ab7d59a8473e13c307812daa8a1d12f5b
Emilie Morissette <emorissettegregoire@gmail.com>
2019-02-21 11:21:38 -0500

    Review model validation done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,18 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :apartment
   has_many :reviews
+
+  validates :enter_date, :exit_date, presence: true
+  validate :exit_date_after_enter_date
+  validate :start_date_cant_be_in_the_past
+
+  private
+
+  def exit_date_after_enter_date
+    errors.add(:exit_date, "Must be after the start date") if exit_date < enter_date
+  end
+
+  def start_date_cant_be_in_the_past
+    errors.add(:enter_date, "Must be from today or forward") if enter_date < (Date.today).to_s
+  end
 end
