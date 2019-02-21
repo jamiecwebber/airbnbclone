48545cd2b9760ac1000dd8f3119fe5d201a2097a
Emilie Morissette <emorissettegregoire@gmail.com>
2019-02-21 17:32:02 -0500

    Playing with Js for slideshow

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -9,6 +9,7 @@ class ApartmentsController < ApplicationController
   # GET /apartments
   def index
     @apartments = Apartment.all
+  end
 
   def show
     @booking = Booking.new
@@ -55,4 +56,3 @@ class ApartmentsController < ApplicationController
     params.require(:apartment).permit(:user_id, :price_per_day, :category, :name, :description, :location, :upload_photos)
   end
 end
-
