dc41344b117cc51b09b07046315e54b35530da97
Emilie Morissette <emorissettegregoire@gmail.com>
2019-02-21 12:26:06 -0500

    Appartment model validation done

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -4,4 +4,6 @@ class Apartment < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :reviews, through: :bookings
+
+  validates :price_per_day, :category, :name, :description, :location, :photos, presence: true
 end
